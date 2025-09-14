be0e3ebe6bd04dfe524ade4cadfb570a8651b853
Serhii Bielik <bielik.sergey@gmail.com>
2025-09-14 16:08:38 +0200

    Add error hadling, db disconnect

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { prisma } from './db';
+import { notFound, errorHandler } from './middleware/error.middleware';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? '3000';
 
-app.listen(port, () => {
-  console.log(`Ready on ${port}`);
-});
+async function main() {
+  app.use(express.json());
+  app.use(notFound);
+  app.use(errorHandler);
+
+  const port = process.env.PORT || '3000';
+
+  app.listen(port, () => {
+    console.log(`Ready on ${port}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
