630ab85bacab89963c9a36559f63e4ac35229f55
Serhii Bielik <bielik.sergey@gmail.com>
2025-09-19 23:16:58 +0200

    Add simple link creation limiter

--- backend/src/routes/link.routes.ts
+++ backend/src/routes/link.routes.ts
@@ -3,11 +3,21 @@ import express from 'express';
 import { createShortLink, getLink } from '../controllers/link.controller';
 import { createLinkSchema } from '../validation/link.validation';
 import { validate } from '../middleware/validate.middleware';
+import rateLimit, { RateLimitRequestHandler } from 'express-rate-limit';
 
 const linkRouter = express.Router();
 
-// linkRouter.route('/shorten').post(createShortLink);
-linkRouter.post('/shorten', validate(createLinkSchema), createShortLink);
+const limiter: RateLimitRequestHandler = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  handler: (_req, _res, next) => {
+    const error = new Error('Too many requests, try again later');
+    (error as any).status = 429;
+    next(error);
+  },
+});
+
+linkRouter.post('/shorten', limiter, validate(createLinkSchema), createShortLink);
 linkRouter.route('/:slug').get(getLink);
 
 export default linkRouter;
